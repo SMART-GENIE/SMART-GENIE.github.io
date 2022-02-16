967ca9188a0d5070a2eb89e832c5b9ad0f802c7d
MAYANK-M31 <mayank31m@gmail.com>
2022-02-16 19:09:41 +0530

    second commit

--- src/Components/Control_Panel/Controlpanel.jsx
+++ src/Components/Control_Panel/Controlpanel.jsx
@@ -1,81 +1,99 @@
-import React from 'react'
-import "./Controlpanel.css"
-import Chart from './Chart';
-import Slidecontent from './Slidecontent';
-import Levels from './Levels';
-import Chart2 from './chart2';
-import { AiFillBell } from 'react-icons/ai'
+import React from "react";
+import "./Controlpanel.css";
+import Chart from "./Chart";
+import Slidecontent from "./Slidecontent";
+import Levels from "./Levels";
+import Chart2 from "./chart2";
+import { AiFillBell } from "react-icons/ai";
 
-function Controlpanel(){
-    return(
-        <div className="controlpanel">
-            <div className="top">
-                <div className="top1">
-                    <br/>
-                    DARK MODE
-                </div>
-                <div className="top2">
-                    <br/>
-                    <span className="noti">NOTIFICATION TELEGRAM</span>
-                    < AiFillBell/>
-                </div>
-            </div>
-            <div className="con">
-                <h1 className="office">Office</h1>
-                <div className="control">
-                <div className="details">
-                <div className="details-1">
-                    <div className="item_detail col1">
-                        <div className="start">
-                            <div className="left">Earned<br/> Ethereum</div>
-                            <div className="right">hi</div>
-                        </div>
-                        <span className="val">0.56 ETH</span>
-                    </div>
-                    <div className="item_detail col2">
-                        <div className="start">
-                            <div className="left">Earned<br/> dollars</div>
-                            <div className="right">hi</div>
-                        </div>
-                        <span className="val">2006.73 USD</span>
-                    </div>
-                </div>
-                <div className="details-1">
-                    <div className="item_detail col3">
-                        <div className="start">
-                            <div className="left">Partners<br/> Toatl/Last 24h</div>
-                            <div className="right">hi</div>
-                        </div>
-                        <span className="val">112/0</span>
-                    </div>
-                    <div className="item_detail col4">
-                        <div className="start">
-                            <div className="left">The current<br/> level</div>
-                            <div className="right">hi</div>
-                        </div>
-                        <span className="val">0 Level</span>
-                    </div>
-                </div>
-
-                    <Slidecontent/>
-                </div>
-
-                <div className="charts">
-                    <div className="graph1">
-                        <Chart/>
-                    </div>
-                    <br/>
-                    <div className="graph1">
-                        <Chart2/>    
-                    </div>
-                </div>
-                
-            </div>
-            </div>
-            <br/><br/>
+function Controlpanel() {
+  return (
+    <div className="controlpanel">
+      <div className="con">
+        <h1 className="office">Office</h1>
+        <div className="control">
             
-            <Levels/>
+          <div className="CoverDiv">
+            <div class="contentcard_tabs_active">
+              <div class="contentcard_tabs_info">
+                <img
+                  src="https://uploads-ssl.webflow.com/5c30d30a32c1877cbb03e545/5c40eb3142d68e3c2b7f8850_ring%20green.png"
+                  width="43"
+                  alt=""
+                  class="contentcard_tabs_active_circle--green"
+                />
+                <div class="contentcard_tabs_active_text_price">
+                  <strong class="bold-text-2">€ 3492,02</strong>
+                </div>
+              </div>
+              <div class="contentcard_tabs_label">Coin Earned</div>
+            </div>
+
+            <div class="contentcard_tabs_active">
+              <div class="contentcard_tabs_info">
+                <img
+                  src="https://uploads-ssl.webflow.com/5c30d30a32c1877cbb03e545/5c43381379dc692ebfb3a76c_ring%20purple.png"
+                  width="43"
+                  alt=""
+                  class="contentcard_tabs_active_circle--green"
+                />
+                <div class="contentcard_tabs_active_text_price">
+                  <strong class="bold-text-2">€ 3492,02</strong>
+                </div>
+              </div>
+              <div class="contentcard_tabs_label">Coin Earned</div>
+            </div>
+
+            <div class="contentcard_tabs_active">
+              <div class="contentcard_tabs_info">
+                <img
+                  src="https://uploads-ssl.webflow.com/5c30d30a32c1877cbb03e545/5c433812ff0f50fa752c8bb5_ring%20blue.png"
+                  width="43"
+                  alt=""
+                  class="contentcard_tabs_active_circle--green"
+                />
+                <div class="contentcard_tabs_active_text_price">
+                  <strong class="bold-text-2">€ 3492,02</strong>
+                </div>
+              </div>
+              <div class="contentcard_tabs_label">Coin Earned</div>
+            </div>
+
+            <div class="contentcard_tabs_active">
+              <div class="contentcard_tabs_info">
+                <img
+                  src="https://uploads-ssl.webflow.com/5c30d30a32c1877cbb03e545/5c40eb3142d68e3c2b7f8850_ring%20green.png"
+                  width="43"
+                  id="LevelImage"
+                  alt=""
+                  class="contentcard_tabs_active_circle--green"
+                />
+                <div class="contentcard_tabs_active_text_price">
+                  <strong class="bold-text-2">€ 3492,02</strong>
+                </div>
+              </div>
+              <div class="contentcard_tabs_label">Coin Earned</div>
+            </div>
+
+          </div>
         </div>
-    )
+        {/* <Slidecontent /> */}
+
+        <div className="charts">
+          <div className="graph1">
+            <Chart />
+          </div>
+          <br />
+          <div className="graph1">
+            <Chart2 />
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+
+      <Levels />
+    </div>
+  );
 }
 export default Controlpanel;
