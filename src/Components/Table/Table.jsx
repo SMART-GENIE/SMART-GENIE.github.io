2d520d20f9986499bcab6bac9b17ea086bc0837e
MAYANK-M31 <mayank31m@gmail.com>
2022-02-17 12:22:56 +0530

    table added

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,51 +1,57 @@
-import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import React from "react"
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-
-export default function Table() {
-  return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-isRowSelectable={()=>false}
-
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-      />
-    </div>
-  );
+const Table = ()=>{
+    return(
+        <div className="TableDivWrap">
+          <div className="rec">
+            <p className="linkname1">Received</p>
+            <div className="search">
+              <div className="inp">
+                <input className="searchinp" placeholder="Search" />
+                <button className="copybtn">Search</button>
+              </div>
+            </div>
+            <div className="recdiv">
+              <table>
+                <tr>
+                  <th>Date</th>
+                  <th>From whom</th>
+                  <th>ID</th>
+                  <th>The amount of ETH</th>
+                  <th>USD</th>
+                </tr>
+                <tr>
+                  <td className="tbval">27.08 17:21</td>
+                  <td className="tbval">
+                    <a href="#">0x9f4f1d2fa3b11e64f21dc121603f31be022e0ae9</a>
+                  </td>
+                  <td className="tbval">1368</td>
+                  <td className="tbval">0.05</td>
+                  <td className="tbval">193.42</td>
+                </tr>
+                <tr>
+                  <td className="tbval">27.08 17:08</td>
+                  <td className="tbval">
+                    <a href="#">0x13c549a61f4aa9065e52858baa2c223fb9d0126e</a>
+                  </td>
+                  <td className="tbval">1367</td>
+                  <td className="tbval">0.03</td>
+                  <td className="tbval">116.05</td>
+                </tr>
+                <tr>
+                  <td className="tbval">27.08 19:40</td>
+                  <td className="tbval">
+                    <a href="#">0xb10bb6ee3247ba91067ae0bb1308e82f687c67e3</a>
+                  </td>
+                  <td className="tbval">1401</td>
+                  <td className="tbval">0.03</td>
+                  <td className="tbval">116.05</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+    )
 }
+
+export default Table
